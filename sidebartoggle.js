899687e30bc1e8cd416f91cea7fb2463e6a182b5
David Zhu <dyzhu12@gmail.com>
2015-11-01 14:18:40 -0500

    Added mouseover sidebar

---
+++ sidebartoggle.js
@@ -0,0 +1,9 @@
+function openSidebar() {
+	var sidebar = document.getElementById('sidebar');
+	sidebar.classList.remove('hidden');
+}
+
+function closeSidebar() {
+	var sidebar = document.getElementById('sidebar');
+	sidebar.classList.add('hidden');
+}
